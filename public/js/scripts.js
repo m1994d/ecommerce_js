be7ad1f2587d2cbb7e6ce074f30f4399acae7c87
Marlon Andrés Diaz Suarez <marlondiaz2994@gmail.com>
2023-10-29 22:21:56 -0500

    fix code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,6 +14,25 @@ window.addEventListener("load", function () {
   let botonesComprar = document.querySelectorAll(".agregar_carrito")
 
   botonesComprar.forEach((boton)=> {
-    //escuchar el cli
+    //escuchar el clic
+    boton.addEventListener("click", (e) => {
+      //console.log(e.target.dataset.id);    
+      if (localStorage.carrito){
+        let carrito = JSON.parse(localStorage.carrito);
+
+        let index = carrito.findIndex((prod) => prod.id == e.target.dataset.id);
+        if (index == -1) {
+          carrito[index].quantity++;
+        } else {
+          carrito.push({id: e.target.dataset.id, quantity: 1});
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        
+      } else {
+        localStorage.setItem("carrito",
+        JSON.stringify([{ id: e.target.dataset.id,quantity: 1}])
+        );
+      }
+    });
   });
 });
